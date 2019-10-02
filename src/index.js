da9d5e1e16aad7e0bbfde5f32bc12adaf99730d6
dmtaddict <dmtaddict@gmail.com>
2019-10-02 15:54:33 +0700

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letter = '';
+    let i = 0;
+    let verb = '';
+    while(i < expr.length){
+        verb = '';
+        if(expr.substr(i , 10) === '**********'){
+            letter += ' ';
+            i+=10;
+            continue;
+        }
+        for(let j = i; j < i+10; j+=2){
+            if(expr.charAt(j) === '0'){
+                continue;
+            }
+            if(expr.charAt(j) === '1' && expr.charAt(j+1) === '1'){
+                verb+='-';
+            }
+            else if(expr.charAt(j) === '1' && expr.charAt(j+1) === '0'){
+                verb+='.';
+            }
+        }
+        if(verb !== ''){
+            letter += MORSE_TABLE[verb];
+        }
+
+        i+=10;
+    }
+    return letter;// write your solution here
 }
 
 module.exports = {
